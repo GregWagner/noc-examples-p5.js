8e2f2f6aa285b783bafc80a33d8894f5a6455b3d
GregWagner <theGreg101@gmail.com>
2020-12-15 11:33:50 -0600

    Spacing and triple equals

--- chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
+++ chp00_introduction/NOC_I_01_RandomWalkTraditional/sketch.js
@@ -5,7 +5,7 @@
 let walker;
 
 function setup() {
-  createCanvas(640,360);
+  createCanvas(640, 360);
   walker = new Walker();
   background(127);
 }
@@ -16,28 +16,28 @@ function draw() {
 }
 
 class Walker {
-  constructor(){
-    this.x = width/2;
-    this.y = height/2;
+  constructor() {
+    this.x = width / 2;
+    this.y = height / 2;
   }
 
   render() {
     stroke(0);
-    point(this.x,this.y);
+    point(this.x, this.y);
   }
 
   step() {
     var choice = floor(random(4));
     if (choice === 0) {
       this.x++;
-    } else if (choice == 1) {
+    } else if (choice === 1) {
       this.x--;
-    } else if (choice == 2) {
+    } else if (choice === 2) {
       this.y++;
     } else {
       this.y--;
     }
-    this.x = constrain(this.x,0,width-1);
-    this.y = constrain(this.y,0,height-1);
+    this.x = constrain(this.x, 0, width - 1);
+    this.y = constrain(this.y, 0, height - 1);
   }
 }
